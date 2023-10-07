05c43832fa90b4339603b36bfac3602e0aeb4e3c
OmarAlsaqa <o.alsaqa@gmail.com>
2023-10-07 17:03:14 +0300

    add toy dataset

--- load_graph.py
+++ load_graph.py
@@ -1,4 +1,6 @@
 import torch as th
+import dgl
+from dgl.data import DGLDataset
 
 def load_data(data):
     g = data[0]
@@ -70,6 +72,9 @@ def load_dataset(dataset_name):
             g.ndata['labels'] = g.ndata['labels'].to(dtype=th.float32)
     elif dataset_name in ['ogbn-products', 'ogbn-arxiv', 'ogbn-papers100M']:
         g, n_classes = load_ogb(dataset_name)
+    elif dataset_name == 'toy':
+        dataset = ToyDataset()
+        g, n_classes, multilabel = dataset[0], 2, multilabel
     else:
         raise ValueError('unknown dataset')
     
@@ -82,3 +87,35 @@ def inductive_split(g):
     val_g = g.subgraph(g.ndata["train_mask"] | g.ndata["val_mask"])
     test_g = g
     return train_g, val_g, test_g
+
+
+class ToyDataset(DGLDataset):
+    def __init__(self):
+        super().__init__(name="toy")
+
+    def process(self):
+        self.graph = dgl.graph(([2, 3, 3, 4], [0, 0, 1, 1] ), num_nodes=5, col_sorted=True)
+        self.graph.ndata["features"] = th.tensor([[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1], [1, 0, 0, 0]] , dtype=th.float32)
+        self.graph.ndata["labels"] = th.tensor([0, 0, 1, 1, 1] , dtype= th.float).type(th.LongTensor)
+        self.graph.edata["weight"] = th.FloatTensor([0.5, 0.5, 0.4, 0.6])
+
+        # If your dataset is a node classification dataset, you will need to assign
+        # masks indicating whether a node belongs to training, validation, and test set.
+        n_nodes = 5
+        n_train = 5
+        n_val = 0
+        train_mask = th.zeros(n_nodes, dtype=th.bool)
+        val_mask = th.zeros(n_nodes, dtype=th.bool)
+        test_mask = th.zeros(n_nodes, dtype=th.bool)
+        train_mask[:n_train] = True
+        val_mask[n_train : n_train + n_val] = True
+        test_mask[n_train + n_val :] = True
+        self.graph.ndata["train_mask"] = train_mask
+        self.graph.ndata["val_mask"] = val_mask
+        self.graph.ndata["test_mask"] = test_mask
+
+    def __getitem__(self, i):
+        return self.graph
+
+    def __len__(self):
+        return 1
